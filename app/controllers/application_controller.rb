028262fea4a011f7007d8abe2ab3f459edf69f78
Sav Younts <savyounts@gmail.com>
2018-09-06 17:03:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/posts/:id' do
+    binding.pry
     @post = Post.find(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
