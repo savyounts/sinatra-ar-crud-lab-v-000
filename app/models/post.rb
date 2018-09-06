270af3ea6926f97fce7db9594c2a3efda121d49e
Sav Younts <savyounts@gmail.com>
2018-09-06 15:44:24 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,5 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
-  attr_accessor :name, :content
 
-
-  def initialize(params)
-    @name = params[:name]
-    @content = params[:content]
-  
-  end
 end
